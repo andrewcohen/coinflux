80d4d9eba595fdbe1751ea3207a80a31d5f54551
Alexzito <413xzito@gmail.com>
2013-12-11 22:06:04 -0500

    Adds basic nav and "delete wallet" modal styles.

--- app/assets/javascripts/bootstrap_includes.js
+++ app/assets/javascripts/bootstrap_includes.js
@@ -1,5 +1,5 @@
 // require bootstrap/affix
-// require bootstrap/alert
+//= require bootstrap/alert
 // require bootstrap/button
 // require bootstrap/carousel
 // require bootstrap/collapse
@@ -7,6 +7,6 @@
 // require bootstrap/tab
 // require bootstrap/transition
 // require bootstrap/scrollspy
-// require bootstrap/modal
+//= require bootstrap/modal
 // require bootstrap/tooltip
 // require bootstrap/popover
